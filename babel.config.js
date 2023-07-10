7229b64d5bfcba6f36305084842e8aa90650dc1c
vitorbisterso <vitorbisterso@gmail.com>
2023-07-10 16:13:21 -0300

    Configure basic eslint and prettier

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-          'module-resolver',
-          {
-            root: ['./src'],
-            extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-            alias: {
-              tests: ['./tests/'],
-              "@": "./",
-            }
-          }
-      ]
-    ]
-  };
+module.exports = function (api) {
+   api.cache(true);
+   return {
+      presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+      plugins: [
+         [
+            'module-resolver',
+            {
+               root: ['./src'],
+               extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+               alias: {
+                  tests: ['./tests/'],
+                  '@': './src',
+               },
+            },
+         ],
+      ],
+   };
 };
